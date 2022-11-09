e95b55f8824420ce1da20c260fd01848050fc8bb
Luiz Fernando <luiz.fernando.l.souza@gmail.com>
2022-11-09 14:29:38 -0300

    Projeto inicial usando o npm

---
+++ Módulo_NodeJs/exec-nodejs/main.js
@@ -0,0 +1,11 @@
+/* Para criar um novo projeto devemos iniciar o comando npm init no terminal, na pasta raiz do projeto.
+
+Logo após iniciar o projeto podemos adicionar a ele pacotes/bibliotecas através do comando npm install nome_do_pacote,
+onde nome_do_pacote será a biblioteca a ser instalada (aqui como teste foi usado a biblioteca Express).
+*/
+
+
+
+const express = require('express');
+const app = express();
+
